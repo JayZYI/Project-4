cbd29551035ec12d3ab16ab032897374d4bf4ec3
JayZYI <zayidanilham7@gmail.com>
2023-07-06 03:06:05 +0700

    android kawe

---
+++ www/Android/app/src/main/java/com/example/project4/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.project4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
